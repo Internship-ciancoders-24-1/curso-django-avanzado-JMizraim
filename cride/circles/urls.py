829153aaef93050ebcc6d4a1246c19d4ff2176a7
JMizraim <jomizraim47@gmail.com>
2024-03-11 09:00:59 -0600

    Día-4, capítulo 4

---
+++ cride/circles/urls.py
@@ -0,0 +1,10 @@
+""" Circles URLs"""
+
+#Django
+from django.urls import path
+from cride.circles.views import list_circles, create_circle
+
+urlpatterns = [
+    path("circles/", list_circles),
+    path("circles/create/", create_circle)
+]
