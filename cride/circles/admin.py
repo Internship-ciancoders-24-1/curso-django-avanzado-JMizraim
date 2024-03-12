021f8a8ec33763b970ac7085b3896728ec7632dd
JMizraim <jomizraim47@gmail.com>
2024-03-12 08:23:29 -0600

    Día-4, capítulo 8

--- cride/circles/admin.py
+++ cride/circles/admin.py
@@ -20,3 +20,20 @@ class CircleAdmin(admin.ModelAdmin):
     )
     search_fields = ('slug_name', 'name')
     list_filter = ('is_public', 'verified', 'is_limited')
+    actions = ['make_verified', 'make_unverified']
+    
+    def make_verified(self, request, queryset):
+        """Make circles verified"""
+        queryset.update(verified=True)
+        
+    make_verified.short_description = 'Verify circle'
+    
+    def make_unverified(self, request, queryset):
+        """Make circles unverified"""
+        queryset.update(verified=False)
+        
+    def download_todays_rides(self, request, queryset):
+        """Download today's rides"""    
+        pass    
+        
+    make_unverified.short_description = 'Unverify circle'
